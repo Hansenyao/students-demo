cd5cd51fb4acfa6f029e1ef2ddf261304d197d37
Youfang yao <yyfsingler@163.com>
2025-09-21 22:03:59 -0600

    finished APIs

---
+++ StudentsServer/StudentsServer/Controllers/StudentsController.cs
@@ -0,0 +1,69 @@
+﻿using StudentsServer.DAL;
+using StudentsServer.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentsServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public StudentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/students
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<student>>> GetStudents()
+        {
+            return await _context.students.ToListAsync();
+        }
+
+        // GET: api/students/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<student>> GetStudent(int id)
+        {
+            var student = await _context.students.FindAsync(id);
+            if (student == null) return NotFound();
+            return student;
+        }
+        // POST: api/students
+        [HttpPost]
+        public async Task<ActionResult<student>> CreateStudent(student student)
+        {
+            _context.students.Add(student);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetStudent), new { id = student.id }, student);
+        }
+
+        // PUT: api/students/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateStudent(int id, student student)
+        {
+            if (id != student.id) return BadRequest();
+
+            _context.Entry(student).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/students/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            var student = await _context.students.FindAsync(id);
+            if (student == null) return NotFound();
+
+            _context.students.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+}
--- StudentsServer/StudentsServer/Program.cs
+++ StudentsServer/StudentsServer/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using StudentsServer.DAL;
+
 namespace StudentsServer
 {
     public class Program
@@ -8,6 +11,8 @@ namespace StudentsServer
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
